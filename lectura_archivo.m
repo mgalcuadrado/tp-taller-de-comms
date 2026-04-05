69d6f3393053fb7e08b9798ee3ab36aff2089fa2
mgalcuadrado <mgamberale@fi.uba.ar>
2026-04-05 00:56:14 -0300

    Codificación de fuente hecha en el archivo lectura_archivo.txt, hay que pasarlo al bloque en algún momento y armar la decodificación de fuente :D

--- lectura_archivo.m
+++ lectura_archivo.m
@@ -1,46 +1,104 @@
 clear;
+clc;
+%abrir_archivo(ruta); DONE
+%char[], proba[] lectura_archivo(archivo); DONE
+%float calcular_entropia_fuente(proba[]); DONE
+%dict huffmandict(); DONE
+%float longitud_minima(dict); DONE
+%float longitud_promedio(dict); DONE
+%palabras[] codificar(archivo, dict); DONE
+nombre_archivo_in = "hola.txt";
+nombre_archivo_out = "chau.txt";
 
-%abrir_archivo(ruta);
-%char[], proba[] lectura_archivo(archivo);
-%float calcular_entropia_fuente(proba[]);
-%dict huffmandict();
-%float longitud_minima(dict);
-%float longitud_promedio(dict);
-%palabras[] codificar(archivo, dict);
-nombre_archivo = "hola.txt";
+archivo = abrir_archivo_lectura(nombre_archivo_in);
 
-disp('Abriendo archivo...')
-archivo = fopen(nombre_archivo, 'r');
-if archivo == -1
-    error("No se pudo abrir el archivo")
+[caracteres, probabilidades, cantidad_caracteres_distintos] = leer_archivo(archivo)
+%verificación de la suma de las probabilidades
+verificacion_suma(probabilidades);
+
+entropia = calcular_entropia_fuente(probabilidades);
+
+disp('Armando el diccionario de Huffman...')
+[dict, avglen] = huffmandict(caracteres, probabilidades)
+disp('Buscando las longitudes mínima y promedio...')
+longitud_minima = entropia / log2(2) % = entropia
+longitud_promedio = avglen
+eficiencia = entropia / avglen;
+
+codificar_archivo(nombre_archivo_in, nombre_archivo_out, dict, cantidad_caracteres_distintos)
+
+function archivo = abrir_archivo_lectura(nombre_archivo);
+    disp('Abriendo archivo...')
+    archivo = fopen(nombre_archivo, 'r');
+    if archivo == -1
+        error("No se pudo abrir el archivo")
+    end
 end
 
-disp('Leyendo el archivo...')
-
-[caracteres, cantidad_apariciones] = leer_archivo(archivo)
-function [caracteres, cantidad_apariciones] = leer_archivo(archivo)
+function [caracteres, probabilidades, cantidad_caracteres_distintos] = leer_archivo(archivo)
+    disp('Leyendo el archivo...')
     cantidad_caracteres_distintos = 0;
     cantidad_caracteres_totales = 0;
     caracteres = 0;
     cantidad_apariciones = 0;
-    caracter = fread(archivo, 1, '*char')
+    caracter = fread(archivo, 1, '*char');
     while caracter ~= char(0)
        % caracter = fread(archivo, 1, '*char')
         caracter_in_caracteres = false;
         for i=1:cantidad_caracteres_distintos
             if caracter == caracteres(i)
-                cantidad_apariciones(i) = cantidad_apariciones(i) + 1
+                cantidad_apariciones(i) = cantidad_apariciones(i) + 1;
                 caracter_in_caracteres = true;
             end
         end 
         if not (caracter_in_caracteres)
-            cantidad_caracteres_distintos = cantidad_caracteres_distintos + 1
-            caracteres(cantidad_caracteres_distintos) = caracter
-            cantidad_apariciones(cantidad_caracteres_distintos) = 1
+            cantidad_caracteres_distintos = cantidad_caracteres_distintos + 1;
+            caracteres(cantidad_caracteres_distintos) = caracter;
+            cantidad_apariciones(cantidad_caracteres_distintos) = 1;
         end
-        cantidad_caracteres_totales = cantidad_caracteres_totales + 1
-    caracter = fread(archivo, 1, '*char')
+        cantidad_caracteres_totales = cantidad_caracteres_totales + 1;
+    caracter = fread(archivo, 1, '*char');
+    end
+    probabilidades = cantidad_apariciones / cantidad_caracteres_totales;
+    fclose(archivo);
+end
+
+function suma = verificacion_suma(probas)
+    suma = 0;
+    for i = 1:length(probas)
+        suma = suma + probas(i);
     end
 end
 
 
+function entropia = calcular_entropia_fuente(probas)
+    disp('Calculando la entropía de la fuente...')
+% entropia = suma (k= 0; n-1) de pk * log2(1/pk)
+    entropia = 0;
+    for i = 1:length(probas)
+        entropia = entropia + probas(i) * log2(1/probas(i));
+    end
+end
+
+function codificar_archivo(nombre_archivo_entrada, nombre_archivo_salida, diccionario, cant_distintos)
+    archivo_entrada = abrir_archivo_lectura(nombre_archivo_entrada);
+    archivo_salida = fopen(nombre_archivo_salida, 'w');
+    caracter = fread(archivo_entrada, 1, '*char');
+    while caracter ~= char(0)
+        posicion_en_dic = 4;
+        for i= 1:cant_distintos
+            dict_char = diccionario{i,1};
+            int_char = uint8(caracter);
+            if dict_char == int_char
+                posicion_en_dic = i;
+            end
+        end
+        %diccionario(posicion_en_dic)
+        palabra = sprintf('%d', diccionario{posicion_en_dic, 2});
+        fwrite(archivo_salida, palabra, 'char');
+        caracter = fread(archivo_entrada, 1, '*char');
+    end
+    fwrite(archivo_salida, char(0), 'char');
+    fclose(archivo_salida);
+    fclose(archivo_entrada);
+end
