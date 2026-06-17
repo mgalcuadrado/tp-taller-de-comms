be632f2416cbfb1eb9e2fb7a281df17bf768cf71
mgalcuadrado <mgamberale@fi.uba.ar>
2026-06-16 22:03:10 -0300

    mi archivito de pruebas

--- pruebas.m
+++ pruebas.m
@@ -1,5 +1,9 @@
 
-
-%prueba_programa_completo("hola.txt", "chau.txt");
-%prueba_programa_completo("entrada.txt", "salida_cod_decod.txt");
-prueba_programa_completo("quijote.txt", "quijote_ruido_de_molinos.txt");
+%%PRUEBAS FUENTE + CANAL
+prueba_codificaciones_decodificaciones_fuente_canal("hola.txt", "chau.txt");
+prueba_codificaciones_decodificaciones_fuente_canal("quixote.txt", "quixote_windmill_sounds.txt");
+%%
+%%PRUEBAS FUENTE + CANAL + MODULACIÓN + EFECTOS DE CANAL
+prueba_programa_completo("hola.txt", "chau.txt");
+prueba_programa_completo("entrada.txt", "salida_cod_decod.txt");
+prueba_programa_completo("quixote.txt", "quixote_windmill_sounds.txt");
